8fd4ea3d36461e22429c28475416053f2c5f7a93
Huwenbo Shi <shihuwenboinus@gmail.com>
2011-05-07 19:49:46 -0700

    Added earthquake information info. Location (city name info) hasn't been integrated yet. It seems that some cities have unusual characters which are hard to present on to the browser. Added a red circle around the earthquake area.

--- modules/quake_parser.py
+++ modules/quake_parser.py
@@ -1,6 +1,8 @@
 import urllib2
 import string
 
+# Returns the earthquake information in a list of tuple.
+# The tuples are in the form: (eqid, time, coords, magnitude, depth, location)
 def quake_parser():
     response = urllib2.urlopen('http://earthquake.usgs.gov/earthquakes/catalogs/eqs7day-M5.txt')
     feed = response.read().split('\n')
@@ -8,8 +10,18 @@ def quake_parser():
     del feed[0]
     del feed[len(feed)-1]
     #build a list of earthquake coordinates
-    coordList = []
+    quakeInfo = []
     for line in feed:
     	splitLine = line.split(',')
-	coordList.append((splitLine[6], splitLine[7]))
-    return coordList
+        eqid = splitLine[1]
+        time = splitLine[3]+", "+splitLine[4]+", " + splitLine[5]
+        time = time.replace("\"","")
+        coords = splitLine[6]+", "+splitLine[7]
+        magnitude = splitLine[8]
+        depth = splitLine[9]
+        location = "".join(splitLine[11:])
+        location = location.replace("\"","")
+        location = location.replace("\'","")
+        info = (eqid, time, coords, magnitude, depth, location)
+        quakeInfo.append(info)
+    return quakeInfo
