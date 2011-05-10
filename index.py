ebe3603503b5b12fa470f8ddf5ad50292d87fd05
Jason Tan <jasontan056@gmail.com>
2011-05-10 02:30:42 -0700

    Updated the map to the current version with the info boxes and magnitude circles.  Replaced the Search with a simple javascript geocoding search.

--- index.py
+++ index.py
@@ -17,72 +17,20 @@ from google.appengine.ext.webapp import template
 class MainPage(webapp.RequestHandler):
     path = os.path.join(os.path.dirname(__file__), 'index.html')
 
+    defaultCoords = "34,-118" 
     def get(self):
-        zoom = 10
-        # The location the user see when s/he opens up the browser.
-        initialLocation = "34.07, -118.44"
-        # The city name the user see when s/he opens up the browser.
-        currentCity = "Los Angeles, CA, USA"
-        # The parsed location is empty at first.
-        isParsedLocationsEmpty = True
-        # init is true when the user opens up the browser.
-        init = True
-        
-        template_values = {
-            'coords': initialLocation,
-            'zoom': zoom,
-            'isParsedLocationsEmpty': isParsedLocationsEmpty,
-            'init': init,
-            'currentCity': currentCity,
-	    'quake_coords': quake_parser()
-        }
-        self.response.out.write(template.render(self.path, template_values))
-
-    def post(self):
-        # inputLocation is the user's location input.
-        inputLocation = self.request.get('search')
-        # cityToWatch is the user's choice when the location is ambiguous
-        cityToWatch = self.request.get('choice')
-        # If the user enters something, init becomes false.
-        init = False
-        zoom = 10
-        
-        # If the user enters nothing the current city keeps the same.
-        currentCity = ""
-        if inputLocation == "":
-            init = True
-            currentCity = "Los Angeles, CA, USA"
-        
-        # Sanitize the inputLocation for Google Map search.
-        sanitizedlocation = sanitize(inputLocation)
-        # Get the data google returns.
-        googleResult = getGoogleResult(sanitizedlocation)
-        # A list of parsed locations.
-        parsedLocations = parseGoogleLocation(googleResult)
-        
-        isParsedLocationsEmpty = False
-        # If parsedLocations is empty set it to false.
-        if len(parsedLocations) == 0:
-            isParsedLocationsEmpty = True
-        
-        # cityToWatch is in (lat, lng|city name) format.
-        coords = ""
-        if cityToWatch != "":
-            coords = cityToWatch.split('|')[0]
-            currentCity = cityToWatch.split('|')[1]
-        else:
-            coords = "34.07, -118.44"
+        coordsToGo = self.request.get('coords')
+        if coordsToGo == "":
+            coordsToGo = self.defaultCoords
 
         template_values = {
-            'coords': coords,
-            'zoom': zoom,
-            'parsedLocations': parsedLocations,
-            'isParsedLocationsEmpty' : isParsedLocationsEmpty,
-            'init': init,
-            'currentCity': currentCity,
-	    'quake_coords': quake_parser()
+            'coordsToGo': coordsToGo,
+            'quake_coords': quake_parser()
         }
+        
         self.response.out.write(template.render(self.path, template_values))
+
+
 
 application = webapp.WSGIApplication([('/', MainPage)],debug=True)
 
--- modules/quake_parser.py
+++ modules/quake_parser.py
@@ -1,17 +1,49 @@
 import urllib2
 import string
 
+# Returns the earthquake information in a list of tuple.
+# The tuples are in the form: (eqid, time, coords, magnitude, depth, location)
 def quake_parser():
-#    response = urllib2.urlopen('http://earthquake.usgs.gov/earthquakes/catalogs/eqs7day-M5.txt')
-    response = urllib2.urlopen('http://earthquake.usgs.gov/earthquakes/catalogs/eqs7day-M2.5.txt')
-
+    response = urllib2.urlopen('http://earthquake.usgs.gov/earthquakes/catalogs/eqs7day-M5.txt')
     feed = response.read().split('\n')
+
     #remove the first and the last lines from the feed
     del feed[0]
     del feed[len(feed)-1]
+
     #build a list of earthquake coordinates
-    coordList = []
+    quakeInfo = []
     for line in feed:
+        # remove the trailing new line character
+        line = line.replace('\n',"")
+
+        # break up the information
     	splitLine = line.split(',')
-	coordList.append((splitLine[6], splitLine[7]))
-    return coordList
+        eqid = splitLine[1]
+
+        # get the earthquake time, remove quotes
+        time = splitLine[3]+", "+splitLine[4]+", " + splitLine[5]
+        time = time.replace("\"","")
+
+        # get coords of the earthquake
+        coords = splitLine[6]+", "+splitLine[7]
+
+        # get magnitude
+        magnitude = splitLine[8]
+
+        # get depth
+        depth = splitLine[9]
+
+        # get location, remove quotes, remove last character
+        location = ",".join(splitLine[11:])
+        location = location.replace("\"","")
+        location = location.replace("\'","\\'")
+        location = location[0:-1]
+
+        # pack all the information up in a tuple
+        info = (eqid, time, coords, magnitude, depth, location)
+
+        # append the tuple to the list
+        quakeInfo.append(info)
+
+    return quakeInfo
