9f4d8faafb18d0af7795c3cb042eab512d938fc4
Huwenbo Shi <shihuwenboinus@gmail.com>
2011-05-17 01:23:49 -0700

    deleted this file because it's not used

--- modules/input_handler.py
+++
@@ -1,5 +0,0 @@
-# Very preliminary user input sanitizer.
-# This function only replaces whitespace with plus sign.
-# The plus sign is used for Google location search.
-def sanitize(input):
-    return input.replace(' ','+')
