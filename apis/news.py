d097c315acc382a082be55b0413ba5da3dcf5815
Huwenbo Shi <shihuwenboinus@gmail.com>
2011-05-22 01:25:03 -0700

    sorry about this

--- apis/news.py
+++ apis/news.py
@@ -1,25 +1,47 @@
 # Python modules
 import os
 import cgi
+import time
+import Cookie
 
-# Google App Engine modules
+# Our own modules
+from modules.tweets_searcher import *
+
+# Google App Engine Modules
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 
-class SearchPage(webapp.RequestHandler):
+class TweetsPage(webapp.RequestHandler):
 
     path = os.path.join(os.path.dirname(__file__), 'news.html')
-
-    def get(self):
     
+    def get(self):
+        cookie = Cookie.SimpleCookie()
+        cookieString = os.environ.get('HTTP_COOKIE')
+        
+        region = ""
+        coords = ""
+        isCookieSet = False
+        
+        if cookieString != None:
+            cookie.load(cookieString)
+            
+            if ('geocode' in cookie) and 'region' in cookie:
+                coords = cookie['geocode'].value
+                coords = coords.replace(" ", "")
+                region = cookie['region'].value
+                isCookieSet = True
         template_values = {
-        }
+                'coords': coords,
+            	'region': region,
+            	'isCookieSet': isCookieSet
+            }
         
         self.response.out.write(template.render(self.path, template_values))
 
-application = webapp.WSGIApplication([('/apis/news.html', SearchPage)],debug=True)
+application = webapp.WSGIApplication([('/apis/news.html', TweetsPage)],debug=True)
 
 def main():
     run_wsgi_app(application)
